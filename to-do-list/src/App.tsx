f7be9f747b455e6934b11bf097c9e5384691a3f5
Hqko01 <hakan.yilmaz0134@hotmail.com>
2025-11-03 23:02:30 +0300

    Basic To Do List Completed

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="title">
+                <p>To Do List With React.js</p>
+            </div>
+            <div className="to-do-card">
+                <div className="el" to-do-id="1">
+                    <div className="to-do-name">
+                        <p>Learn React.js</p>
+                    </div>
+                    <ToDoCheckbox id="1" />
+                </div>
+                <div className="el" to-do-id="2">
+                    <div className="to-do-name">
+                        <p>Learn Typescript</p>
+                    </div>
+                    <ToDoCheckbox id="2" />
+                </div>
+                <div className="el" to-do-id="3">
+                    <div className="to-do-name">
+                        <p>Learn Next.js</p>
+                    </div>
+                    <ToDoCheckbox id="3" />
+                </div>
+                <div className="el" to-do-id="4">
+                    <div className="to-do-name">
+                        <p>Learn Tailwind CSS</p>
+                    </div>
+                    <ToDoCheckbox id="4" />
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+function ToDoCheckbox({ id }: { id: string }) {
+    function toDo() {
+        const el = document.querySelector(`[to-do-id="${id}"]`);
+        el?.classList.toggle('active');
+    }
+
+    return (
+        <div className="to-do-checker" onClick={toDo}></div>
+    );
 }
 
 export default App;
